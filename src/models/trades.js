bbc18169b16e5a47699515df09a837893a3645ee
Debayan Ghosh <debayanghosh2406@gmail.com>
2024-08-11 00:31:26 +0530

    fixed the get-balance endpoint to fetch correct the values

--- src/models/trades.js
+++ src/models/trades.js
@@ -12,16 +12,6 @@ const TradeSchema = new mongoose.Schema({
     price: Number
 });
 
-// Schema for the Balance at a specific timestamp for a currency
-const BalanceSchema = new mongoose.Schema({
-    userId: Number,
-    currency: String,
-    balance: Number,
-    utcTime: Date
-});
-
 // Create a model based on the schema
 const Trade = mongoose.model('Trade', TradeSchema);
-const Balance = mongoose.model('Balance', BalanceSchema);
 exports.Trade = Trade;
-exports.Balance = Balance;
